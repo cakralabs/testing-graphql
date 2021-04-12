4c7c53991466072470ba3f3a1db8aa02f5d3971a
Cakra Aminuddin <cakra.amin@yahoo.com>
2021-04-12 15:38:34 +0700

    initial

---
+++ graphql/schemas/index.js
@@ -0,0 +1,15 @@
+// graphql/schemas/index.js
+
+const { gql } = require('apollo-server-express');
+
+const rootType = gql`
+ type Query {
+     root: String
+ }
+ type Mutation {
+     root: String
+ }
+
+`;
+
+module.exports = [rootType];
